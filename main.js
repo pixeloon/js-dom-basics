6fba03057c79c7d6b2a5d734b8bbe6c28aed0b29
Gunther Schneider <pixeloon@gmail.com>
2016-02-25 13:41:07 -0800

    changed td variable names

--- main.js
+++ main.js
@@ -12,27 +12,24 @@ var animals = [
 var table = document.createElement("table");
 var tbody = document.createElement("tbody");
 var tr;
-var td;
+var td1, td2;
 
 // append table to DOM
 document.querySelector('body').appendChild(table);
 table.appendChild(tbody);
 
 for (var i = 0; i < animals.length; i++) {
+    // 1. create tr 
   tr = document.createElement("tr");
-      td = document.createElement("td");
-      td.innerText = animals[i].name;
-      tr.appendChild(td);
-      td = document.createElement("td");
-      td.innerText = animals[i].sound;
-      tr.appendChild(td);
-
-  tbody.appendChild(tr);
-
-  // 1. create tr 
   // 2. for each tr, create two tds
-  // 3. for each td, modify the innerText
-  // 4. append the tds to the tr
-  // 5. append the tr to the tbody
-
+      td1 = document.createElement("td");
+      // 3. for each td, modify the innerText
+      // 4. append the tds to the tr
+      td1.innerText = animals[i].name;
+      tr.appendChild(td1);
+      td2 = document.createElement("td");
+      td2.innerText = animals[i].sound;
+      tr.appendChild(td2);
+      // 5. append the tr to the tbody
+  tbody.appendChild(tr);
 }
